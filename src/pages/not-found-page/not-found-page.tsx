37a900469d2843eff972367e5a9a41986153503c
Abramof Andrey <abramofandrey2@gmail.com>
2023-10-22 00:08:28 +0300

    обновит ссылку `<a>` на `<Link>` из пакета `react-router-dom` на странице NotFoundPage для перехода на MainPage

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,11 +1,18 @@
+import { Link } from 'react-router-dom';
+
 import styles from './not-found-page.module.css';
+import { AppRoute } from '../../const';
 
 function NotFoundPage() {
   return (
     <div className={`page ${styles.notFoundPage}`}>
       <title>{'6 cities - Not Found'}</title>
       <h1 className={styles.title}>404 Not Found</h1>
-      <p className={styles.text}>Return to the main page
+      <p className={styles.text}>
+        Return to the{' '}
+        <Link to={AppRoute.Root} className={styles.link}>
+          main page
+        </Link>
       </p>
     </div>
   );
