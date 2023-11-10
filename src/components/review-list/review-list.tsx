87164a29591e28fbefa5c6f3924fc4195c7a5edc
Abramof Andrey <abramofandrey2@gmail.com>
2023-11-11 01:09:12 +0300

    Добавит компонент 'Review'

--- src/components/review-list/review-list.tsx
+++ src/components/review-list/review-list.tsx
@@ -1,4 +1,5 @@
 import { ReviewType } from '../../types/review';
+import Review from '../review/review';
 
 type TReviewList = {
   reviews: ReviewType[];
@@ -10,24 +11,7 @@ function ReviewList({ reviews }: TReviewList) {
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
       <ul className="reviews__list">
         {reviews.map((review) => (
-          <li key={review.id} className="reviews__item">
-            <div className="reviews__user user">
-              <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                <img className="reviews__avatar user__avatar" src={review.user.avatarUrl} width="54" height="54" alt="Reviews avatar" />
-              </div>
-              <span className="reviews__user-name">{review.user.name}</span>
-            </div>
-            <div className="reviews__info">
-              <div className="reviews__rating rating">
-                <div className="reviews__stars rating__stars">
-                  <span style={{ width: `${(review.rating / 5) * 100}%` }}></span>
-                  <span className="visually-hidden">Rating</span>
-                </div>
-              </div>
-              <p className="reviews__text">{review.comment}</p>
-              <time className="reviews__time" dateTime={review.date}>{review.date}</time>
-            </div>
-          </li>
+          <Review key={review.id} review={review}/>
         ))}
       </ul>
     </>
